20822c6cefab1c22dd58af52199b0e67c8113bef
jonasmanthey <jgmanthey@gmail.com>
2020-10-15 22:41:31 +0200

    more vuetify removals

--- src/main.js
+++ src/main.js
@@ -1,12 +1,4 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import vuetify from '@/plugins/vuetify'
 
 createApp(App).mount("#app");
-
-app.use(vuetify, i18nStrings)
-app.use(vuetify, i18nStrings)
-
-new Vue({
-    vuetify,
-  }).$mount('#app')
