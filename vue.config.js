e74dfc751fc9ac154fb27b1f5c9108dfba922dff
aktentasche <jonas@amps.one>
2020-10-22 18:34:52 +0200

    added public path on amps server

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
 
+  publicPath: process.env.NODE_ENV === "production" ? "/cv/" : "/",
+
   pluginOptions: {
     i18n: {
       locale: "en",
