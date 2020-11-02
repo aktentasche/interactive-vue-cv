c4e149de75064ee1a700808c3761b88fa6478f1c
aktentasche <jonas@amps.one>
2020-11-02 17:59:32 +0100

    added vuetify and csharp, also sorting of skill entries

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -114,7 +114,18 @@ export default {
               }
             );
 
-      console.log(toreturn);
+      toreturn.sort((a, b) => {
+        let fa = a.name.toLowerCase(),
+          fb = b.name.toLowerCase();
+
+        if (fa < fb) {
+          return -1;
+        }
+        if (fa > fb) {
+          return 1;
+        }
+        return 0;
+      });
       return toreturn;
     }
   }
