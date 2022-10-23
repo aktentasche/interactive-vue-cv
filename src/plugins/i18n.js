7efd38a4d0b501128ea5b2132506838f90e47724
Jonas Manthey <jonas.manthey@psi.ch>
2022-10-23 19:38:18 +0200

    more highlights, translations

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -3,23 +3,23 @@ import VueI18n from "vue-i18n";
 
 Vue.use(VueI18n);
 
-const available_short_isos = ["en", "dk", "ger"];
+const available_short_isos = ["en", "dk"];
 
 const suffixes = [
   "-base",
   "-education",
   "-professional-experience",
   "-projects",
-  "-skills"
+  "-skills",
 ];
 
 function getLocaleMessages() {
   var messages = {};
-  available_short_isos.forEach(locale => {
+  available_short_isos.forEach((locale) => {
     // init with no content
     messages[locale] = {};
     //append json content
-    suffixes.forEach(suffix => {
+    suffixes.forEach((suffix) => {
       messages[locale] = Object.assign(
         require("./../content/" + locale + suffix + ".json"),
         messages[locale]
@@ -32,5 +32,5 @@ function getLocaleMessages() {
 export default new VueI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || "en",
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en",
-  messages: getLocaleMessages()
+  messages: getLocaleMessages(),
 });
