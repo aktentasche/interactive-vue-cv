402914da8c1c2c1d3b144029364d822e1c23c8ae
Jonas Manthey <jonas.manthey@psi.ch>
2022-10-23 19:31:31 +0200

    removed german, add more highlights, removed picture for now

--- src/components/about-me.vue
+++ src/components/about-me.vue
@@ -11,13 +11,13 @@
     <!-- px3 to align with title -->
     <v-card-text class="px-3">
       <v-row>
-        <v-img
+        <!-- <v-img
           src="me.jpg"
           min-height="300px"
           max-height="500px"
           min-width="200px"
           id="aboutmeimg"
-        />
+        /> -->
       </v-row>
 
       <h2 class="pt-2 pb-2">{{ $t("about_me_headline") }}</h2>
@@ -29,9 +29,51 @@
         </div>
       </v-row>
 
+      <!-- language -->
+      <h2 class="pt-2 pb-2">{{ $t("language_levels") }}</h2>
+      <v-divider class="pb-2" />
+      <table>
+        <tr>
+          <td>
+            <v-img width="30px" src="flag_images/DE.svg" class="ml-1"></v-img>
+          </td>
+          <td>
+            <v-icon class="pl-4" size="28">mdi-star</v-icon>
+            <v-icon v-for="index in 4" :key="index" size="28">mdi-star</v-icon>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <v-img width="30px" src="flag_images/GB.svg" class="ml-1"></v-img>
+          </td>
+          <td>
+            <v-icon class="pl-4" size="28">mdi-star</v-icon>
+            <v-icon v-for="index in 4" :key="index" size="28">mdi-star</v-icon>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <v-img width="30px" src="flag_images/FR.svg" class="ml-1"></v-img>
+          </td>
+          <td>
+            <v-icon class="pl-4" size="28">mdi-star</v-icon>
+            <v-icon v-for="index in 1" :key="index" size="28">mdi-star</v-icon>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <v-img width="30px" src="flag_images/DK.svg" class="ml-1"></v-img>
+          </td>
+          <td>
+            <v-icon class="pl-4" size="28">mdi-star</v-icon>
+            <v-icon v-for="index in 0" :key="index" size="28">mdi-star</v-icon>
+          </td>
+        </tr>
+      </table>
+
+      <!-- contact -->
       <h2 class="pt-2 pb-2">{{ $t("contact") }}</h2>
       <v-divider class="pb-2" />
-      <!-- contact -->
       <v-icon class="mr-2">mdi-email-outline</v-icon>
       <a target="empty" :href="'mailto:' + emailaddress">{{ emailaddress }}</a>
       <br />
@@ -57,14 +99,14 @@ import LanguageSwitcher from "./language-switcher";
 
 export default {
   components: {
-    LanguageSwitcher,
+    LanguageSwitcher
   },
   data() {
     return {
       emailaddress: "jonas@amps.one",
       phoneNumber: "+41 76 677 113 37",
-      phoneNumbeIsHidden: true,
+      phoneNumbeIsHidden: true
     };
-  },
+  }
 };
 </script>
